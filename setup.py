0b15b419d643fefd02f8947e51557ab5619f8e89
Pradyumna <kopallepradyumna@gmail.com>
2025-09-16 16:52:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Generative AI project',
+    version='0.0.0',
+    author='Sai Pradyumna',
+    author_email='kopallepradyumna@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
